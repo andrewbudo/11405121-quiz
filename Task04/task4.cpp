c6e49acb74e6ce72a3c97871a6b615003827acb3
nikita <nikita.alexxandrovich@gmail.com>
2023-12-14 13:16:39 +0300

    Task4

---
+++ Task04/task4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int pannels;
+	int a;
+	int b;
+	int gramms;
+	cin >> pannels >> a >> b;
+	gramms = ((a*b)*2)*pannels;
+	cout << gramms << endl;
+}
