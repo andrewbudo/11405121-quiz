05cdb42bf39bc4b3d05c26805bdf828a8d6c49fb
Shuteyka <aleksandr.shut.olegovich@gmail.com>
2023-12-14 13:51:41 +0300

    4.cpp

--- Task04/4.cpp
+++ Task04/4.cpp
@@ -1,3 +1,12 @@
-q
+#include <iostream>
+using namespace std;
 
+int main() {
+  int N, A, B;
+  cin >> N >> A >> B;
 
+  int totalArea = N * A * B * 2;
+  cout << totalArea << endl;
+
+  return 0;
+}
